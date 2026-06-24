1f653f2ff4daa94b559bb0900353a0a0586574c3
Luis Rogelio <luisrogelio98@gmail.com>
2026-06-24 14:23:43 +0200

    headers update

--- tools/form_to_spreadsheet/flutter_bootstrap.js
+++ tools/form_to_spreadsheet/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1355413021" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3656911962" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
